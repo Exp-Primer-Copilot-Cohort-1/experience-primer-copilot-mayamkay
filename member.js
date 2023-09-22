95315149f58caa185833ddf1ba86088fcc28731f
Maya Mallaby-Kay <63798503+mayamkay@users.noreply.github.com>
2023-09-22 20:59:30 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,22 @@
+function skillsMember() {
+    var member = document.getElementById("member");
+    var skills = document.getElementById("skills");
+    var contact = document.getElementById("contact");
+    var about = document.getElementById("about");
+    var home = document.getElementById("home");
+    var memberContent = document.getElementById("memberContent");
+    var skillsContent = document.getElementById("skillsContent");
+    var contactContent = document.getElementById("contactContent");
+    var aboutContent = document.getElementById("aboutContent");
+    var homeContent = document.getElementById("homeContent");
+    member.style.backgroundColor = "#ff6f00";
+    skills.style.backgroundColor = "#ff6f00";
+    contact.style.backgroundColor = "#ff6f00";
+    about.style.backgroundColor = "#ff6f00";
+    home.style.backgroundColor = "#ff6f00";
+    memberContent.style.display = "block";
+    skillsContent.style.display = "none";
+    contactContent.style.display = "none";
+    aboutContent.style.display = "none";
+    homeContent.style.display = "none";
+}
